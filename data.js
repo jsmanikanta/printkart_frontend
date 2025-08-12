7dee99522fa931cee165e78d0cb7eb5fac5872f4
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2025-08-12 20:11:17 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-export const api_path = "https://printkart-backend.onrender.com";
-=======
-//export const api_path = "http://localhost:5000";
+
 export const api_path="https://printkart-backend.onrender.com";
->>>>>>> 54dce20969548c24c595d01bd0986a46e2c25e07
