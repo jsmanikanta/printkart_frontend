af9872e72e2f92374cd8627d449e7b2bd4231f9f
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2025-08-12 19:22:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // set this according to your deployment path
-  build: {
-    chunkSizeWarningLimit: 2000, // Increase chunk size limit in KB if you want
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'
-          }
-        },
-      },
-    },
-  },
-})
+  base: './', // or "", as needed for your deployment
+});
+
