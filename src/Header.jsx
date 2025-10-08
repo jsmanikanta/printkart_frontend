2648a02da747cd81a55f47b10adcd688dadf52a0
J S Manikanta <jsmanikanta13@gmail.com>
2025-10-08 19:38:25 +0530

    new UI1

--- src/Header.jsx
+++ src/Header.jsx
@@ -61,7 +61,6 @@ function Header() {
           <span className="black">Book</span>
           <span className="gray">Hub</span>
         </h1>
-
         <div className="header-actions">
           <div className="user-profile" onClick={goToLogin}>
             <div className="user-avatar">
@@ -69,12 +68,10 @@ function Header() {
             </div>
             <span className="login-text">{userName ? userName : "Login"}</span>
           </div>
-
           <div className="help-section" onClick={goToHelp}>
             <img src="/images/help-icon.png" alt="Help" className="help-icon" />
             <span className="help-text">Help</span>
           </div>
-
           <div className="location-section" onClick={goToLocations}>
             <img
               src="/images/location-icon.png"
@@ -83,7 +80,6 @@ function Header() {
             />
             <span className="location-text">Delivery Location</span>
           </div>
-
           <div className="cart-section" onClick={goToCart}>
             <img src="/images/cart-icon.png" alt="Cart" className="cart-icon" />
             <span className="cart-text">Cart</span>
