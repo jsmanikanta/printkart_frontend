02c2c04937e2b3d09933078d027071cfea8d3f02
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2025-10-18 19:20:35 +0530

    Update Orderprints.jsx

--- src/Orderprints.jsx
+++ src/Orderprints.jsx
@@ -91,7 +91,6 @@ export default function OrderPrints() {
 
     let pricePerPage = 0;
     if (color === "b/w" && sides === "2") pricePerPage = 1;
-    else if (color === "colour" && sides === "2") pricePerPage = 2.5;
     else if (color === "b/w" && sides === "1") pricePerPage = 1.5;
     else if (color === "colour" && sides === "1") pricePerPage = 6;
 
