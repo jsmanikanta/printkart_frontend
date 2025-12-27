af33e9ad955d0ce36fd0313dd98186f62a7db426
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2025-12-27 20:26:27 +0530

    Update Bottomnav.jsx

--- src/Bottomnav.jsx
+++ src/Bottomnav.jsx
@@ -50,7 +50,7 @@ function BottomNav() {
 
       <button
         className={`nav-item ${isActive("/sellbooks") ? "active" : ""}`}
-        onClick={() => navigate("/sellbook")}
+        onClick={() => navigate("/sellbooks")}
         aria-label="Sell Now"
       >
         <img src="/images/sell-now-icon.png" alt="Sell Now" />
